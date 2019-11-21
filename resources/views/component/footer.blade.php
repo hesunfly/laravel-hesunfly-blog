a9e814510057cdea6b145d408ae5e966318d54c4
hesunfly <hesunfly@163.com>
2019-11-21 09:33:30 +0800

    footer

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="border-t border-lighter mt-20">
         <div class="container mx-auto px-5 lg:max-w-screen">
             <div class="text-muted py-10 text-center">
-                
+                Copyright © <a href="http://hesunfly.com" style="text-decoration: none">Hesunfly</a> | <a href="http://www.beian.miit.gov.cn" style="text-decoration: none">陕ICP备17022875号</a>
             </div>
         </div>
     </div>
