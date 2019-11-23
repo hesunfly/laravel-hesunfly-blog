f0c313c7259f435b530dbd23f0a40cb2536cccae
hesunfly <hesunfly@163.com>
2019-11-23 13:55:31 +0800

    edit icon

--- resources/views/admin/component/sidebar.blade.php
+++ resources/views/admin/component/sidebar.blade.php
@@ -17,40 +17,35 @@
         <li class="sidebar-nav-heading">文章</li>
         <li class="sidebar-nav-link">
             <a href="{{ url('/admin/articles/write') }}" @if ($uri == '/admin/articles/write') class="active" @endif >
-                <i class="am-icon-home sidebar-nav-link-logo"></i> 写作
+                <i class="am-icon-edit sidebar-nav-link-logo"></i> 写作
             </a>
         </li>
         <li class="sidebar-nav-link">
             <a href="{{ url('/admin/articles') }}" @if ( $uri != '/admin/articles/write' && mb_strstr($uri, '/admin/articles')) class="active" @endif >
-                <i class="am-icon-table sidebar-nav-link-logo"></i> 文章
+                <i class="am-icon-files-o sidebar-nav-link-logo"></i> 文章
             </a>
         </li>
         <li class="sidebar-nav-link">
             <a href="{{ url('/admin/categories') }}" @if (mb_strstr($uri, '/admin/categories')) class="active" @endif>
-                <i class="am-icon-calendar sidebar-nav-link-logo"></i> 分类
+                <i class="am-icon-folder-o sidebar-nav-link-logo"></i> 分类
             </a>
         </li>
         <li class="sidebar-nav-link">
             <a href="{{ url('/admin/pages') }}" @if (mb_strstr($uri, '/admin/pages')) class="active" @endif>
-                <i class="am-icon-bar-chart sidebar-nav-link-logo"></i> 页面
+                <i class="am-icon-file-code-o sidebar-nav-link-logo"></i> 页面
 
             </a>
         </li>
 
         <li class="sidebar-nav-heading">资源</li>
         <li class="sidebar-nav-link">
-            <a href="{{ url('/admin/members') }}">
-                <i class="am-icon-key sidebar-nav-link-logo"></i> 用户
-            </a>
-        </li>
-        <li class="sidebar-nav-link">
             <a href="{{ url('/admin/images') }}">
-                <i class="am-icon-tv sidebar-nav-link-logo"></i> 图片
+                <i class="am-icon-file-image-o sidebar-nav-link-logo"></i> 图片
             </a>
         </li>
         <li class="sidebar-nav-link">
             <a href="{{ url('/admin/files') }}">
-                <i class="am-icon-tv sidebar-nav-link-logo"></i> 文件
+                <i class="am-icon-file-o sidebar-nav-link-logo"></i> 文件
             </a>
         </li>
 
