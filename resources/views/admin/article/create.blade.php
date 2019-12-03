87824eb97589aca8bd58888dbe1632b6d119bcdb
hesunfly <hesunfly@163.com>
2019-12-03 12:56:12 +0800

    取消常见文章自动保存

--- resources/views/admin/article/create.blade.php
+++ resources/views/admin/article/create.blade.php
@@ -122,11 +122,6 @@
 
         let simplemde = new SimpleMDE({
             autoDownloadFontAwesome: undefined,
-            autosave: {
-                enabled: true,
-                uniqueId: target.data('save-id'),
-                delay: 1000,
-            },
             element: document.getElementById('content'),
             insertTexts: {
                 image: ["![](", ")"],
