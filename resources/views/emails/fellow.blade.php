b1cc4c7b7f1682f90ce99d1730648876ee2696d5
hesunfly <hesunfly@163.com>
2019-12-21 13:05:30 +0800

    修复邮件内容

--- resources/views/emails/fellow.blade.php
+++ resources/views/emails/fellow.blade.php
@@ -83,7 +83,7 @@
                                                                    style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
                                                                 <tr>
                                                                     <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                                                                        <a href="http://hesunfly.test/articles/article_5dd604b6f3cf0"
+                                                                        <a href="{{ url('/articles/' . $article->slug) }}"
                                                                            class="button button-blue" target="_blank"
                                                                            style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; border-radius: 3px; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.16); color: #FFF; display: inline-block; text-decoration: none; -webkit-text-size-adjust: none; background-color: #3097D1; border-top: 10px solid #3097D1; border-right: 18px solid #3097D1; border-bottom: 10px solid #3097D1; border-left: 18px solid #3097D1;">阅读文章</a>
                                                                     </td>
