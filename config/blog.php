8d21448d8d5fc9b06ebc115fca3a97e08e0f373f
hesunfly <hesunfly@163.com>
2020-03-24 23:51:11 +0800

    分页和样式

--- config/blog.php
+++ config/blog.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'page_size' => 10,
+    'page_size' => 15,
     'admin_page_size' => 15,
     'icp_record' => '陕ICP备17022875号',
     'reward_code_img' => '/assets/images/reward_code_img.png',
