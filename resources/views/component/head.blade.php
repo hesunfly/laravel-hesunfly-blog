3d5823ac04da0fd3727564aed1562efc476d36c7
hesunfly <hesunfly@163.com>
2020-06-06 16:56:43 +0800

    静态资源cdn

--- resources/views/component/head.blade.php
+++ resources/views/component/head.blade.php
@@ -7,12 +7,12 @@
     <meta name="theme-color" content="#011627">
     <link rel="icon" type="image/png" href="/favicon.ico">
     <link rel="stylesheet" href="/assets/css/css.css">
-    <link href="https://upyun.cdn.hesunfly.com/css/theme.css" rel="stylesheet">
+    <link href="https://upyun.cdn.hesunfly.com/assets/css/theme.css" rel="stylesheet">
     <link href="/assets/css/search.css" rel="stylesheet">
     <link href="/assets/admin/css/github.min.css" rel="stylesheet">
     <link href="/assets/css/github-markdown-css.css" rel="stylesheet">
     <link href="/assets/admin/css/font-awesome.min.css" rel="stylesheet">
-    <script src="https://upyun.cdn.hesunfly.com/js/jquery.min.js"></script>
+    <script src="https://upyun.cdn.hesunfly.com/assets/js/jquery.min.js"></script>
     <script src="/assets/axios.min.js"></script>
     <script src="/assets/layer/layer.js"></script>
 
