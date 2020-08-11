9f109b8d93658eba649ac2f6c277035567dcb686
hesunfly <hesunfly@163.com>
2020-08-11 18:37:53 +0800

    edit subscribes list page

--- resources/views/admin/subscribes.blade.php
+++ resources/views/admin/subscribes.blade.php
@@ -30,16 +30,40 @@
                                     <thead>
                                     <tr>
                                         <th>邮箱</th>
+                                        <th>状态</th>
                                         <th>次数</th>
                                         <th>时间</th>
+                                        <th>操作</th>
                                     </tr>
                                     </thead>
                                     <tbody>
                                     @foreach ($subscribes as $item)
                                         <tr class="gradeX">
                                             <td>{{ $item->email }}</td>
+                                            <td>
+                                                @if ($item->status == 1)
+                                                    <span style="color: green;font-weight: bold;">启用</span>
+                                                @else
+                                                    <span style="color: #8c8c8c;font-weight: bold;">关闭</span>
+                                                @endif
+                                            </td>
                                             <td>{{ $item->times }}</td>
                                             <td>{{ $item->created_at->toDatetimeString() }}</td>
+                                            <td>
+                                                <div class="tpl-table-black-operation">
+                                                    <a href="{{ url('/articles/') . '/' . $item->slug}}" target="_blank"
+                                                       style="border: 1px solid orange;color: orange;">
+                                                        <i class="am-icon-eye"></i> 查看
+                                                    </a>
+                                                    <a href="{{ url('/admin/articles/edit') . '/' . $item->id }}">
+                                                        <i class="am-icon-pencil"></i> 编辑
+                                                    </a>
+                                                    <a href="javascript:;" onclick="destroy({{ $item->id }})"
+                                                       class="tpl-table-black-operation-del">
+                                                        <i class="am-icon-trash"></i> 删除
+                                                    </a>
+                                                </div>
+                                            </td>
                                         </tr>
                                     @endforeach
                                     </tbody>
@@ -69,6 +93,48 @@
         </div>
     </div>
 </div>
+<script>
+    $(function () {
+        $('.is_publish').attr('disabled', true);
+
+        $('#search').click(function () {
+            search();
+        });
+
+        $('#category_select').change(function () {
+            search();
+        });
+    });
+
+    function destroy(id) {
+        if (!id) {
+            return false;
+        }
+
+        layer.confirm('确定删除吗？', {
+            title: '⚠️',
+            btn: ['删除', '取消'] //按钮
+        }, function () {
+            axios.delete("{{ url('/admin/articles/destroy') }}" + '/' + id)
+                .then(function (response) {
+                    layer.msg('删除成功！', {
+                            time: 1000 //2秒关闭（如果不配置，默认是3秒）
+                        }, function () {
+                            window.location = "{{ url('/admin/articles/') }}";
+                        }
+                    );
+                })
+                .catch(function (error) {
+                    layer.msg('error！', {
+                            time: 1000 //2秒关闭（如果不配置，默认是3秒）
+                        }, function () {
+                            return false;
+                        }
+                    );
+                });
+        });
+    }
+</script>
 @component('admin/component/foot')
 @endcomponent
 
